6ed6c76a4a0f786ce03728739721ad46be759b25
brahmantyo <brahmantyo.adi@gmail.com>
2015-09-10 23:55:08 +0700

    second commit

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION)){
 
 // site domain name with http
 defined("SITE_URL")
-    || define("SITE_URL","http://".$_SERVER['SERVER_NAME']."/admin");
+    || define("SITE_URL","http://".$_SERVER['SERVER_NAME']."/xbook/admin");
 
 // directory separator
 defined("DS")
@@ -63,9 +63,9 @@ set_include_path(implode(PATH_SEPARATOR, array(
 defined("DB_HOST")
     || define("DB_HOST", "localhost");
 defined("DB_USER")
-    || define("DB_USER", "ataicoe");
+    || define("DB_USER", "root");
 defined("DB_PASSWORD")
-    || define("DB_PASSWORD", "viadolorosa~!@");
+    || define("DB_PASSWORD", "");
 defined("DB_NAME")
     || define("DB_NAME", "xbookdb");
     
