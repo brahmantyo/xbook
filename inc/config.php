f3a16a7e9a01b29f8d02013fc273cca9a4957f7c
brahmantyo <brahmantyo.adi@gmail.com>
2015-09-12 09:01:00 +0700

    sesuaikan config

--- inc/config.php
+++ inc/config.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION)){
 
 // site domain name with http
 defined("SITE_URL")
-    || define("SITE_URL","http://".$_SERVER['SERVER_NAME']."/xbook/");
+    || define("SITE_URL","http://".$_SERVER['SERVER_NAME']."/");
 
 // directory separator
 defined("DS")
