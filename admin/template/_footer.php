e6a6f9212e4a19929f3da8702bcadbf7757e7efa
brahmantyo <brahmantyo.adi@gmail.com>
2015-09-30 02:04:04 +0700

    last update

--- admin/template/_footer.php
+++ admin/template/_footer.php
@@ -3,7 +3,7 @@
         </div>
             <div id="footer" class="badge">
                 <span class="pull-left">Copyright 2015@Niclogic - Bandung</span>
-                <span class="pull-right">(Last Update: 26 Juni 2015)</span>
+                <span class="pull-right">(Last Update: 30 September 2015)</span>
             </div>
     </body>
 </html>
